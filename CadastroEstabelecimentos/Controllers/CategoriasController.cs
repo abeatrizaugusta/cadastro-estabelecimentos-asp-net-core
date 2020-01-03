11bf45142a78240e2efd978ed3788f305fddc4bf
Amanda Beatriz <12121002076@muz.ifsuldeminas.edu.br>
2020-01-02 23:22:31 -0300

    Crud Categoriass

--- CadastroEstabelecimentos/Controllers/CategoriasController.cs
+++ CadastroEstabelecimentos/Controllers/CategoriasController.cs
@@ -1,24 +1,145 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using CadastroEstabelecimentos.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using CadastroEstabelecimentos.Models;
 
 namespace CadastroEstabelecimentos.Controllers
 {
     public class CategoriasController : Controller
     {
-        public IActionResult Index()
-        {
-            List<Categoria> list = new List<Categoria>();
-            list.Add(new Categoria { Id = 1, Nome = "Supermercado" });
-            list.Add(new Categoria { Id = 2, Nome = "Restaurante" });
-            list.Add(new Categoria { Id = 3, Nome = "Borracharia" });
-            list.Add(new Categoria { Id = 4, Nome = "Posto" });
-            list.Add(new Categoria { Id = 5, Nome = "Oficina" });
 
-            return View(list);
+        private readonly CadastroEstabelecimentosContext _context; //Dependência do DBContext do EntityFramework
+
+        public CategoriasController(CadastroEstabelecimentosContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Categoria.ToListAsync());
+        }
+
+        //Detalhes
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var categoria = await _context.Categoria
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (categoria == null)
+            {
+                return NotFound();
+            }
+
+            return View(categoria);
+        }
+
+        // Método Criar
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+       
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Nome")] Categoria categoria)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(categoria);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(categoria);
+        }
+
+        // Editar
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var categoria = await _context.Categoria.FindAsync(id);
+            if (categoria == null)
+            {
+                return NotFound();
+            }
+            return View(categoria);
+        }
+
+        
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Nome")] Categoria categoria)
+        {
+            if (id != categoria.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(categoria);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CategoriaExists(categoria.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(categoria);
+        }
+
+        //Deletar
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var categoria = await _context.Categoria
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (categoria == null)
+            {
+                return NotFound();
+            }
+
+            return View(categoria);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var categoria = await _context.Categoria.FindAsync(id);
+            _context.Categoria.Remove(categoria);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool CategoriaExists(int id)
+        {
+            return _context.Categoria.Any(e => e.Id == id);
         }
     }
 }
---
+++ CadastroEstabelecimentos/Data/CadastroEstabelecimentosContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace CadastroEstabelecimentos.Models
+{
+    public class CadastroEstabelecimentosContext : DbContext
+    {
+        public CadastroEstabelecimentosContext (DbContextOptions<CadastroEstabelecimentosContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<CadastroEstabelecimentos.Models.Categoria> Categoria { get; set; }
+    }
+}
