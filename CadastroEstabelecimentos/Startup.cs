9e9f17a62d1e38b9624f5af9e3a8982daacadf9e
Amanda Beatriz <12121002076@muz.ifsuldeminas.edu.br>
2020-01-02 23:57:42 -0300

    Configurando Mysql e primeira migration

--- CadastroEstabelecimentos/Startup.cs
+++ CadastroEstabelecimentos/Startup.cs
@@ -1,10 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -22,16 +18,15 @@ namespace CadastroEstabelecimentos
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
 
             services.AddDbContext<CadastroEstabelecimentosContext>(options =>
-                    options.UseSqlServer(Configuration.GetConnectionString("CadastroEstabelecimentosContext")));
+                    options.UseMySql(Configuration.GetConnectionString("CadastroEstabelecimentosContext"), builder =>
+                        builder.MigrationsAssembly("CadastroEstabelecimentos")));    
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -41,7 +36,6 @@ namespace CadastroEstabelecimentos
             else
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
             app.UseHttpsRedirection();
